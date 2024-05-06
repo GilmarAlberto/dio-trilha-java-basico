dd542971c8601060e9fa014619e70657ee03e362
Gilmar Alberto <gilabreu00@gmail.com>
2024-05-06 05:11:11 -0300

    Trilha Java Básico

---
+++ sistema-smart-tv/src/Usuario.java
@@ -0,0 +1,40 @@
+public class Usuario {
+    public static void main(String[] args) throws Exception {
+        SmartTv smartTv = new SmartTv();
+
+        System.out.println("Tv ligada? " + smartTv.ligada);
+        System.out.println("Canal atual: " + smartTv.canal);
+        System.out.println("Volume atual: " + smartTv.volume);
+
+        smartTv.ligar();
+
+        System.out.println("Tv ligada? " + smartTv.ligada);
+
+        smartTv.desligar();
+        System.out.println("Tv ligada? " + smartTv.ligada);
+
+        smartTv.aumentarVolume();
+        System.out.println("Volume atual: " + smartTv.volume);
+
+        smartTv.aumentarVolume();
+        System.out.println("Volume atual: " + smartTv.volume);
+
+        smartTv.diminuirVolume();
+        System.out.println("Volume atual: " + smartTv.volume);
+
+        smartTv.mudarCanal(13);
+        System.out.println("Canal atual: " + smartTv.canal);
+
+        smartTv.aumentarCanal();
+        System.out.println("Canal atual: " + smartTv.canal);
+
+        smartTv.mudarCanal(45);
+        System.out.println("Canal atual: " + smartTv.canal);
+
+        smartTv.diminuirCanal();
+        System.out.println("Canal atual: " + smartTv.canal);
+
+
+        
+    }
+}
