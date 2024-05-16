c2efdb2ef4db08f134bc185d5fb2ad3d6f20a829
Gilmar Alberto <gilabreu00@gmail.com>
2024-05-16 07:27:37 -0300

    Modelando o iPhone com UML

---
+++ desafio uml iphone/Iphone.java
@@ -0,0 +1,4 @@
+// Classe Iphone.java
+public class Iphone implements ReprodutorMusical, AparelhoTelefonico, NavegadorInternet {
+    // Implementação dos métodos das interfaces
+}
---
+++ desafio uml iphone/NavegadorInternet.java
@@ -0,0 +1,6 @@
+// Interface NavegadorInternet.java
+public interface NavegadorInternet {
+    void exibirPagina(String url);
+    void adicionarNovaAba(String url);
+    void atualizarPagina();
+}
